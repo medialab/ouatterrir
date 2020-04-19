ef35ae143cd2d064cf2013fd6247787aa46c8dd4
Yomguithereal <guillaumeplique@gmail.com>
2020-04-19 20:23:57 +0200

    More scaffolding

--- api/server.js
+++ api/server.js
@@ -1,10 +1,26 @@
+const async = require('async');
 const express = require('express');
 const bodyParser = require('body-parser');
 const Fingerprint = require('express-fingerprint');
 const MongoClient = require('mongodb').MongoClient;
 
+const PORT = 3000;
+
 const DB_URL = 'mongodb://localhost:27017';
 const DB_NAME = 'ouatterrir';
+
+let MONGO_CLIENT;
+let ANSWERS;
+
+function connect(callback) {
+  const client = new MongoClient(DB_URL);
+
+  return client.connect((err) => {
+    if (err) return callback(err);
+
+    return callback(null, client);
+  });
+}
 
 const app = express();
 
@@ -19,3 +35,35 @@ app.use(
     ]
   })
 );
+
+function timestampMiddleware(req, res, next) {
+  req.timestamp = +new Date();
+  return next();
+}
+
+app.use(timestampMiddleware);
+
+function start(callback) {
+  async.series(
+    [
+      (next) => {
+        connect((err, client) => {
+          if (err) return next(err);
+
+          MONGO_CLIENT = client;
+          const db = client.db(DB_NAME);
+
+          ANSWERS = db.collection('answers');
+        });
+      },
+      async.apply(app.listen, PORT)
+    ],
+    callback
+  );
+}
+
+start((err) => {
+  if (err) return console.error(err);
+
+  console.log(`Server listening to port ${PORT}`);
+});
