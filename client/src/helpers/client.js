d40b540166e514f1c6ac9dc90b3ed9ff72d55bc4
Robin de Mourat <robin.demourat@gmail.com>
2020-04-22 15:34:10 +0200

    use api url

--- client/src/helpers/client.js
+++ client/src/helpers/client.js
@@ -1,6 +1,11 @@
-import {post} from 'axios';
+import axios from 'axios';
 
+const API_URL = process.env.API_URL || '//localhost:4000/';
 
 export const postAnswer = data => {
-  return post('/answer', data)
+  return axios({
+    method: 'POST',
+    url: `${API_URL}answer`,
+    data: { data }
+  })
 }
