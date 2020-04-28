be76d219dfa1585adb9376f0c33500d69b203e52
Robin de Mourat <robin.demourat@gmail.com>
2020-04-28 15:44:33 +0200

    Updates

---
+++ precache-manifest.27383b4c5f09b9c1b85b49e65269910e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca7f2b9dc2e2a144f56e1013ced7bd2",
+    "url": "/ouatterrir/index.html"
+  },
+  {
+    "revision": "bc7baf88ddc3124bcf65",
+    "url": "/ouatterrir/static/css/main.ef970b49.chunk.css"
+  },
+  {
+    "revision": "5258a5a61b0105326064",
+    "url": "/ouatterrir/static/js/2.af7888fe.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/ouatterrir/static/js/2.af7888fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc7baf88ddc3124bcf65",
+    "url": "/ouatterrir/static/js/main.28a6897a.chunk.js"
+  },
+  {
+    "revision": "63be50df1262e723bb7f",
+    "url": "/ouatterrir/static/js/runtime-main.b0d5f4d1.js"
+  }
+]);
